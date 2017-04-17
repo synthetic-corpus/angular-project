6a40a30f60de07395d26abc08119303223ecdee7
jgonzaga <joel.gonzaga@example.com>
2017-04-17 11:43:09 -0700

    Made the outline that I wanted.

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipelistComponent } from './recipelist/recipelist.component';
+import { RecipeitemComponent } from './recipeitem/recipeitem.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { ShoppingeditComponent } from './shoppingedit/shoppingedit.component';
+import { RecipedetailsComponent } from './recipe/recipedetails/recipedetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipeComponent,
+    ShoppinglistComponent,
+    ShoppingeditComponent,
+    RecipedetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ project/src/app/recipe/recipedetails/recipedetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipedetails',
+  templateUrl: './recipedetails.component.html',
+  styleUrls: ['./recipedetails.component.css']
+})
+export class RecipedetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
