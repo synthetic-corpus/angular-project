23d5a602992c8529af93176e4461fb7d7a67e636
jgonzaga <joel.gonzaga@example.com>
2017-04-17 17:22:48 -0700

    Added a new model class. Added corresponding array in Recipe list.

--- project/src/app/recipe/recipelist/recipelist.component.ts
+++ project/src/app/recipe/recipelist/recipelist.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipelist',
   templateUrl: './recipelist.component.html',
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test Stew','This is only a test','http://www.clipartkid.com/images/523/plate-of-chocolate-chip-cookies-clip-art-WuAWqU-clipart.jpg')
+  ];
 
   constructor() { }
 
