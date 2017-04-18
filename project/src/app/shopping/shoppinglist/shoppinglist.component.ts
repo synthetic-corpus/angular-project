31dc4dda1f2056d33ec20ad0fb50cf0fde9e7ab2
jgonzaga <joel.gonzaga@example.com>
2017-04-18 13:12:14 -0700

    modified shopping list.

--- project/src/app/shopping/shoppinglist/shoppinglist.component.ts
+++ project/src/app/shopping/shoppinglist/shoppinglist.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../../shared/ingredient.model'
+
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.css']
 })
 export class ShoppinglistComponent implements OnInit {
+  ingredients = [
+    new Ingredient ('Apple',3,''),
+    new Ingredient ('Sugar',3,'Pounds')
+  ];
 
   constructor() { }
 
