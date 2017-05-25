bd961b7d279129e0b3b23a6db73d9219894c4e6a
Syn <joel.gonzaga@gmail.com>
2017-05-24 17:01:03 -0700

    added different pictures and more recipes.

--- project/src/app/recipe/recipelist/recipelist.component.ts
+++ project/src/app/recipe/recipelist/recipelist.component.ts
@@ -7,7 +7,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipelistComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Stew','This is only a test','http://maxpixel.freegreatpicture.com/static/photo/2x/Plate-Cookies-Chocolate-Cookies-Confectionery-1103792.jpg')
+    new Recipe('A test Stew','This is only a test','https://cdn.pixabay.com/photo/2014/08/26/15/24/sausage-428067_960_720.jpg'),
+    new Recipe('Cookies','This is not real','https://cdn.pixabay.com/photo/2016/03/18/02/16/cookies-1264263_960_720.jpg'),
+    new Recipe('Beef','This is not real','https://static.pexels.com/photos/161481/appetite-banana-calories-catering-161481.jpeg')
   ];
   @Output('listSays') message = new EventEmitter<Recipe>();
 
