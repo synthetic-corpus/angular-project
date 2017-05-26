52a7dc1764e5bc4d85916a67b4ea1abb3b098841
Syn <joel.gonzaga@gmail.com>
2017-05-26 16:13:24 -0700

    have added the right services and ingedients about this project.

--- project/src/app/recipe/recipe.model.ts
+++ project/src/app/recipe/recipe.model.ts
@@ -1,9 +1,15 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  // An empty array of Ingredients.
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string){
+  // When this model is constructed, it will now take an array of ingredients too.
+  constructor(name: string, description: string, imagePath: string,
+              ingredients: Ingredient[]){
     this.name = name;
     this.description = description;
     imagePath = imagePath;
--- project/src/app/recipe/recipe.service.ts
+++ project/src/app/recipe/recipe.service.ts
@@ -3,9 +3,23 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   private recipes: Recipe[] = [
-    new Recipe('A test Stew','This is only a test','https://cdn.pixabay.com/photo/2014/08/26/15/24/sausage-428067_960_720.jpg'),
-    new Recipe('Cookies','This is not real','https://cdn.pixabay.com/photo/2016/03/18/02/16/cookies-1264263_960_720.jpg'),
-    new Recipe('Beef','This is not real','https://static.pexels.com/photos/161481/appetite-banana-calories-catering-161481.jpeg')
+    new Recipe('A test Stew','This is only a test','https://cdn.pixabay.com/photo/2014/08/26/15/24/sausage-428067_960_720.jpg',
+    [
+      new Ingredient('Beef',2),
+      new Ingredient('Thyme',4),
+      new Ingredient('Carrots',5)
+    ]),
+    new Recipe('Cookies','This is not real','https://cdn.pixabay.com/photo/2016/03/18/02/16/cookies-1264263_960_720.jpg',
+    [
+        new Ingredient('Coconut',2),
+        new Ingredient('Oatmeal',4),
+        new Ingredient('Sugar',5)
+    ]),
+    new Recipe('Beef','This is not real','https://static.pexels.com/photos/161481/appetite-banana-calories-catering-161481.jpeg',
+    [
+      new Ingredient('Dead Cow', 1),
+      new Ingredient('Moo',1)
+    ])
   ];
   getRecipes() {
     return this.recipes.slice();
