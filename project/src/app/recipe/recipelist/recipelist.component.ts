f028f9e063683952a16f0982123ec2591b5a4e2a
Syn <joel.gonzaga@gmail.com>
2017-05-26 16:50:39 -0700

    kinda sorta have routs working for recipe item details.

--- project/src/app/recipe/recipelist/recipelist.component.ts
+++ project/src/app/recipe/recipelist/recipelist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipelist',
