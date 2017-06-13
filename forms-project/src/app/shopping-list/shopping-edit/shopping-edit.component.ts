a386a49e75e8b74de9d8be24b272b981ab0db542
Syn <joel.gonzaga@gmail.com>
2017-06-12 17:32:59 -0700

    Removed commented out code. Adjusted the TS scipt to remove viewchild.

--- forms-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ forms-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -15,28 +15,21 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  /*@ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;*/
-  @ViewChild('x') xForm: NgForm;
-  // nameInput: string;
-  // amountInput: number;
-
-
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
   onAddItem(Form: NgForm) {
-    const ingName = this.xForm.form.value.nameInput;
-    const ingAmount = this.xForm.form.value.amountInput;
+    const ingName = Form.value.nameInput;
+    const ingAmount = Form.value.amountInput;
     const newIngredient = new Ingredient(ingName, ingAmount);
     console.log(newIngredient);
     this.slService.addIngredient(newIngredient);
   }
 
-  onClear(){
-    this.xForm.reset();
+  onClear(Form: NgForm){
+    Form.reset();
   }
 
 }
