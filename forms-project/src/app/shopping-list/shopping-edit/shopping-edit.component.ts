62625a25ff32fc1f838dc82b6f51e03cae7807e9
Syn <joel.gonzaga@gmail.com>
2017-06-12 17:16:03 -0700

    added a template form to add ingredients.

--- forms-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ forms-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,6 +4,7 @@ import {
   ElementRef,
   ViewChild
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -14,18 +15,23 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  /*@ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;*/
+  @ViewChild('x') xForm:NgForm;
+  // nameInput: string;
+  // amountInput: number;
+
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
+  onAddItem(Form: NgForm) {
+    const ingName = this.xForm.form.value.nameInput;
+    const ingAmount = this.xForm.form.value.amountInput;
     const newIngredient = new Ingredient(ingName, ingAmount);
+    console.log(newIngredient);
     this.slService.addIngredient(newIngredient);
   }
 
