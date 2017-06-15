b45ccd7e11de47bd76f83fc88e5bcd6084768451
Syn <joel.gonzaga@gmail.com>
2017-06-14 17:15:03 -0700

    fixed the display columns for ingredients. Verified that it works.

--- forms-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ forms-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -12,6 +12,7 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService ) { }
@@ -35,18 +36,33 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImgPath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       var recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImgPath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if(recipe['ingredients']){
+        // If there are ingredients, add that ingriends to the array.
+        // Format them so that they are Form Array objects, not the custom
+        // ingredient object.
+        for ( let ingredient of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imgPath': new FormControl(recipeImgPath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
   }
 
